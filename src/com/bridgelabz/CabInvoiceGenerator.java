af7254d4a079548571fbad03843679cee4f4ed5f
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-13 12:55:56 +0530

    added: Logic for multiple ride

--- src/com/bridgelabz/CabInvoiceGenerator.java
+++ src/com/bridgelabz/CabInvoiceGenerator.java
@@ -7,28 +7,28 @@ public class CabInvoiceGenerator {
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
     private static final int MIN_FARE = 5;
+
     public double CalculateFare(double distance, int time) {
-        /**
-         * formula for total fare
-         */
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
         if (totalFare < MIN_FARE) {
             return MIN_FARE;
         }
         return totalFare;
     }
-
+    public double calculateFareForMultipleRides(Ride[] rides) {
+        // variable
+        double totalFare = 0.0;
+        /**
+         * using for each loop
+         */
+        for (Ride ride : rides) {
+            totalFare = CalculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+    }
     public static void main(String[] args) {
-        /**
-         * create object for CabInvoiceGenerator class, object name is cabInvoiceGenerator
-         */
+
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        /**
-         * calling CalculateFare method from cabInvoiceGenerator object
-         * and print o/p
-         */
         System.out.println(cabInvoiceGenerator.CalculateFare(5, 20) + " Rs");
     }
-
 }
-
