bb8e0361f9e271272006fe8e4784ba2aefd2366e
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-13 13:09:02 +0530

    added: Logic invoic service

--- src/com/bridgelabz/CabInvoiceGenerator.java
+++ src/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,46 +1,18 @@
 package com.bridgelabz;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CabInvoiceGenerator {
-
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
     private static final int MIN_FARE = 5;
-
-    /**
-     * create a method name as CalculateFare, this is parameterized method
-     * Created a method to calculate the fare of journey
-     *
-     * @param distance is 5
-     * @param time
-     * @return total fare
-     */
     public double calculateFare(double distance, int time) {
-        /**
-         * formula for total fare
-         */
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
-        /**
-         * if total fare is greater than min fare then
-         */
         if (totalFare < MIN_FARE) {
-            /**
-             * return minimum fare
-             */
             return MIN_FARE;
         }
-        /**
-         * return total fare
-         */
         return totalFare;
     }
-
-    /**
-     * Creating a parameterized method name as calculateFareForMultipleRides
-     * Take a multiple ride and calculate the aggregate total for all
-     *
-     * @param rides multiple ride
-     * @return total fare
-     */
     public double calculateFareForMultipleRides(Ride[] rides) {
         // variable
         double totalFare = 0.0;
@@ -50,22 +22,75 @@ public class CabInvoiceGenerator {
         for (Ride ride : rides) {
             totalFare = calculateFare(ride.getDistance(), ride.getTime());
         }
-        /**
-         * return total fare
-         */
         return totalFare;
     }
 
     /**
-     * @param rides
-     * @return
+     * create a method name as invoiceSummaryCalculation
+     *
+     * @param rides total no of rides
+     * @return invoice summaray calculation return total ride
      */
     public InvoiceSummery invoiceSummaryCalculation(Ride[] rides) {
+        //variable
         double totalFare = 0.0;
+        /**
+         * using for each loop for total fare
+         */
         for (Ride ride : rides) {
             totalFare += calculateFare(ride.getDistance(), ride.getTime());
         }
+        /**
+         * return total invoice summary
+         */
         return new InvoiceSummery(rides.length, totalFare);
+    }
+
+    /**
+     * create a method name as getInvoice
+     *
+     * @param userId given user id
+     * @return tatal rides array
+     */
+    public InvoiceSummery getInvoice(int userId) {
+        /**
+         * crete a map object
+         */
+        Map<Integer, Ride[]> map = new HashMap<>();
+        /**
+         * 1st ride
+         */
+        Ride[] rides1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        /**
+         * 2nd ride
+         */
+        Ride[] rides2 = {new Ride(5.0, 10),
+                new Ride(1, 1)
+        };
+
+        /**
+         * 3rd ride
+         */
+        Ride[] rides3 = {new Ride(8.0, 15),
+                new Ride(1, 10)
+        };
+        /**
+         * calling put method from map object
+         */
+        map.put(1, rides1);
+        map.put(2, rides2);
+        map.put(3, rides3);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+            if (userId == entry.getKey()) {
+                System.out.println(entry.getKey());
+                Ride[] ridesArray = entry.getValue();
+                return invoiceSummaryCalculation(ridesArray);
+            }
+        }
+        return null;
     }
 
 
@@ -84,5 +109,18 @@ public class CabInvoiceGenerator {
          * and print o/p
          */
         System.out.println(cabInvoiceGenerator.calculateFare(5, 20) + " Rs");
+
+        /**
+         * rides array = distance,time
+         */
+        Ride[] ridesArray = {
+                new Ride(0.1, 1),
+                new Ride(1, 1),
+                new Ride(1, 10)
+        };
+        /**
+         * calling getInvoice method from cabInvoiceGenerator object
+         */
+        cabInvoiceGenerator.getInvoice(1);
     }
 }
--- src/com/bridgelabz/InvoiceSummery.java
+++ src/com/bridgelabz/InvoiceSummery.java
@@ -5,20 +5,47 @@ public class InvoiceSummery {
     int numberOfRides;
     double totalFare;
     double avgFare;
+    private double invoiceSummery;
 
+    /**
+     * create a default constructor name as InvoiceSummary
+     */
     public InvoiceSummery() {
 
     }
-    public InvoiceSummery(int numberOfRides, double totalFare) {
-        this.numberOfRides = numberOfRides;
-        this.totalFare = totalFare;
-    }
-    public InvoiceSummery(int numberOfRides, double totalFare, String type) {
 
+    /**
+     *  create a parameterized constructor name as InvoiceSummary
+     * @param numberOfRides in cab invoice generator
+     * @param totalFare in cab invoice generator
+     */
+    public InvoiceSummery(int numberOfRides, double totalFare) {
+        /**
+         *  The "this" keyword is used to refer to the current object.
+         */
         this.numberOfRides = numberOfRides;
         this.totalFare = totalFare;
     }
-    public InvoiceSummery getInvoiceSummery() {
+
+    /**
+     * create a parameterized constructor name as InvoiceSummary
+     * @param numberOfRides in cab invoice generator
+     * @param totalFare in cab invoice generator
+     * @param type in cab invoice generator
+     */
+    public InvoiceSummery(int numberOfRides, double totalFare, String type) {
+        /**
+         *  The "this" keyword is used to refer to the current object.
+         */
+        this.numberOfRides = numberOfRides;
+        this.totalFare = totalFare;
+    }
+
+    /**
+     * Average Fare Per Ride
+     * @return avgFare
+     */
+    public InvoiceSummery getInvoiceSummary() {
         /**
          *  The "this" keyword is used to refer to the current object.
          *  formula = avgFare =totalFare / numberOfRides
@@ -26,6 +53,14 @@ public class InvoiceSummery {
         this.avgFare = this.totalFare / this.numberOfRides;
         return this;
     }
+
+    /**
+     * Create a boolean type method name as equals
+     * to compare objects we should have hashcode method else they
+     * create a two different instances so creating equals() and hashcode()
+     * @param o object
+     * @return number of rides
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -33,8 +68,22 @@ public class InvoiceSummery {
         InvoiceSummery that = (InvoiceSummery) o;
         return numberOfRides == that.numberOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.avgFare, avgFare) == 0;
     }
+
+    /**
+     * overriding the toString() method
+     * @return firstname , numberOfRides ,totalFare, avgFare
+     *
+     */
     @Override
     public int hashCode() {
         return Objects.hash(numberOfRides, totalFare, avgFare);
+    }
+
+    public double getInvoiceSummery() {
+        return invoiceSummery;
+    }
+
+    public void setInvoiceSummery(double invoiceSummery) {
+        this.invoiceSummery = invoiceSummery;
     }
 }
